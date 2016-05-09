9656dcf389d8b866650e7eb13fca73f0152b0a5e
hamham300 <hamham300@naver.com>
2016-05-10 01:39:06 +0900

    Add files via upload

---
+++ ball.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+int main(void)
+{
+	int pin = 0, pin2 = 0, pin3 = 0, prame = 1, score = 0, spare =0, strike = 0;
+	while (prame < 10)
+	{
+		printf("%d번째 프레임 1번째 처리 핀 수: ",prame);
+	    scanf("%d", &pin);
+        if (pin == 10)
+		{
+			score += 10;
+			
+		    if (spare == 1)
+			{
+				score += 10;
+				spare --;
+			}
+			if (strike == 1)
+			{
+				score += 10;
+			}
+			if (strike == 2)
+			{
+				score += 10;
+				score += 10;
+				strike--;
+			}
+		    strike++;	
+		}
+		if (pin < 10)
+		{
+			if (strike == 0 && spare == 0)
+			{
+				score += pin;
+			     
+			}
+		    if (spare == 1)
+			{
+				score += pin;
+				score += pin;
+				spare--;
+			} 
+			if (strike == 1)
+			{
+				score += pin;
+				score += pin;
+			}
+			if (strike == 2)
+			{
+				score += pin;
+				score += pin;
+				score += pin;
+				strike--;
+			}
+		    printf("**** 현재까지 점수 :%d\n", score);
+		    printf("%d번째 프레임 2번째 처리 핀 수: ",prame);
+            scanf("%d", &pin2);
+            if( strike == 0)
+			{
+			    score += pin2;
+			}
+			if ( strike == 1)
+			{
+				score += pin2;
+				score += pin2;
+				strike--;
+			}
+			if (pin + pin2 == 10)
+			{
+				spare++;
+			}
+		 }
+		printf("**** 현재까지 점수 :%d\n", score);
+	    prame++;
+	}
+    if( prame == 10)
+      { 
+		printf("10번째 프레임 1번째 처리 핀 수: ");
+        scanf("%d", &pin);
+		score += pin;
+		printf("**** 현재까지 점수 :%d\n", score);
+		printf("10번째 프레임 2번째 처리 핀 수: ");
+        scanf("%d", &pin2);
+			score += pin2;
+		if ( pin != 10 && pin2 != 10 && pin+pin2 == 10 )
+		printf("**** 최종점수  :%d\n", score);
+		if ( pin == 10 || pin2 == 10 || pin+pin2 == 10)
+		printf("10번째 프레임 3번째 처리 핀 수: ");
+        scanf("%d", &pin3);
+			score += pin3;
+		printf("**** 최종점수  :%d\n", score);
+
+
+			
+      }
+	return 0;
+}
